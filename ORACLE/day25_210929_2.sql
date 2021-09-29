d45506c8a19c8b5e228b301c961d1d8162c28c98
YejiKim <kej980324@gmail.com>
2021-09-29 15:05:01 +0900

    [day25] wip

--- ORACLE/day25_210929_2.sql
+++ ORACLE/day25_210929_2.sql
@@ -1,3 +1,17 @@
 -- subQuery
 select * from emp2;
 select * from emp2 where name='백원만';
+
+
+-- 강의장 왔다!
+-- 서브쿼리의 결과가 한 개 이상 반환될 때는 in, any, all 연산자를 사용함.
+select empno, name, deptno
+    from emp2
+    where deptno in (1000, 1001, 1002, 1010, 2000, 2001);
+    
+select dcode from dept2 where area='서울지사';
+
+select empno, name, deptno
+    from emp2
+    where deptno in (select dcode from dept2 where area='서울지사');
+
--- ORACLE/day25_quiz.sql
+++ ORACLE/day25_quiz.sql
@@ -41,14 +41,11 @@ select ename, decode(comm, null, 'no commission', comm) commission
 -- 1. EMP2 테이블을 사용하여 전체 직원 중 과장 직급의 최소 연봉자보다 연봉이 
 --    높은 사람의 이름과 직급,연봉을 출력하세요. 
 --    단 연봉 출력 형식은 아래와 같이 천 단위 구분기호와 원 표시를 하세요
-
 select name, position, to_char(pay, 'L999,999,999')
     from emp2
     where pay > (select min(pay) from emp2 where position='과장');
 
 -- 2. student 테이블을 조회하여 전체 학생 중에서 체중이 4학년 학생들의 체중에서 가장 적게 나가는 학생보다 적은 학생의 이름과 학년과 몸무게를 출력.
-select * from student;
-
 select name, grade, weight
     from student
     where weight < (select min(weight) from student where grade=4);
@@ -58,16 +55,10 @@ select grade, name, height
     from student
     where (grade, height) in (select grade, max(height) from student group by grade);
 
-select grade, max(height) from student group by grade;
-
 -- 4. professor 테이블을 조회하여 각 학과별로 입사일이 가장 오래된 교수의 교수번호와 이름, 입사일, 학과명을 출력. 
 --     단, 학과명순으로 오름차순 정렬.
-select * from department;
-select * from professor;
 select p.profno, p.name, p.hiredate, d.dname
     from professor p join department d
-    on p.deptno=d.deptno;
-    
--- 학과별로 입사일이 가장 오래된 사람
-select deptno, cast(months_between(sysdate, hiredate) as int)
-    from professor ;
+    on p.deptno=d.deptno
+    where hiredate in (select min(hiredate) from professor group by deptno)
+    order by d.dname asc;
